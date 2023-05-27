39dae43df07a66a2e6e94757c42d6d218b999797
Yoshiyuki Tsunomoto <wasiwasimaru@gmail.com>
2023-05-27 12:42:07 +0900

    update 設定の取得方法を追加。

---
+++ DotNetAdobePdfServiceSample.Lib/AdobePdfServiceSettings.cs
@@ -0,0 +1,33 @@
+﻿namespace DotNetAdobePdfServiceSample.Lib
+{
+    /// <summary>
+    /// <see cref="AdobePdfService"/> の設定
+    /// </summary>
+    public class AdobePdfServiceSettings
+    {
+        /// <summary>
+        /// ClientId
+        /// </summary>
+        public string? ClientId { get; set; }
+
+        /// <summary>
+        /// ClientSecret
+        /// </summary>
+        public string? ClientSecret { get; set; }
+
+        /// <summary>
+        /// OrganizationId
+        /// </summary>
+        public string? OrganizationId { get; set; }
+
+        /// <summary>
+        /// AccountId
+        /// </summary>
+        public string? AccountId { get; set; }
+
+        /// <summary>
+        /// PrivateKey
+        /// </summary>
+        public string? PrivateKey { get; set; }
+    }
+}
--- DotNetAdobePdfServiceSample.Lib/ExecutionContextFactory.cs
+++ DotNetAdobePdfServiceSample.Lib/ExecutionContextFactory.cs
@@ -17,8 +17,24 @@ namespace DotNetAdobePdfServiceSample.Lib
         /// <param name="rootPath">設定ファイルのルートパス</param>
         public ExecutionContextFactory(string rootPath)
         {
+            // NOTE pdfservices-api-credentials.json と private.key は同一階層である必要があります。
             _credentials = Credentials.ServiceAccountCredentialsBuilder()
                 .FromFile(Path.Combine(rootPath, "pdfservices-api-credentials.json"))
+                .Build();
+        }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="settings"><see cref="AdobePdfServiceSettings"/></param>
+        public ExecutionContextFactory(AdobePdfServiceSettings settings)
+        {
+            _credentials = Credentials.ServiceAccountCredentialsBuilder()
+                .WithClientId(settings.ClientId)
+                .WithClientSecret(settings.ClientSecret)
+                .WithOrganizationId(settings.OrganizationId)
+                .WithAccountId(settings.AccountId)
+                .WithPrivateKey(settings.PrivateKey)
                 .Build();
         }
 
